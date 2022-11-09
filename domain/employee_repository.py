ad63ff182665e3de88d156d2e1586c5ba814e800
tristan <tristacha59@gmail.com>
2022-11-09 15:14:08 +0100

    refacto(remove employee repository and change imports)

--- domain/employee_repository.py
+++
@@ -1,6 +0,0 @@
-class EmployeeRepository:
-    def __init__(self, list_employees) -> None:
-        self.list_employees = list_employees
-
-    def get_all_employees(self):
-        return self.list_employees
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from time import strftime
 from birthday_service import BirthdayService
-from data_file_adapter import DataFileAdapter
-from email_service import EmailService
-from domain.employee_repository import EmployeeRepository
+from infra.data_file_adapter import DataFileAdapter
+from domain.email_service import EmailService
 
 
 def main():
