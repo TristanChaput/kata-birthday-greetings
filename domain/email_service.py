db9c6c8e83acca64e4135928136d57f634ecb05e
tristan <tristacha59@gmail.com>
2022-11-09 15:20:34 +0100

    move email service to domain

---
+++ domain/email_service.py
@@ -0,0 +1,3 @@
+class EmailService:
+    def send_email(self, first_name: str, email: str):
+        print("Subject: Happy birthday! Happy birthday, dear " + first_name)
--- email_service.py
+++
@@ -1,3 +0,0 @@
-class EmailService:
-    def send_email(self, first_name: str):
-        print("Subject: Happy birthday! Happy birthday, dear " + first_name)
